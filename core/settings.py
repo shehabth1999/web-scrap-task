adac2064fc67d282caaf84b8572ce104435dd20a
shehab <shethr999@gmail.com>
2024-05-19 03:58:05 +0300

    Added Cloud Database Create requirements and runtime files

--- core/settings.py
+++ core/settings.py
@@ -80,12 +80,25 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xesgosac',
+        'USER': 'xesgosac',
+        'PASSWORD': 'M725pCyu9IIZoe5azOp9QNhz58_QJRiY',
+        'HOST': 'lucky.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
